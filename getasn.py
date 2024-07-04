fcba394588712ec79bce90922fe68353c23e4c28
tizianoghisu <t.ghisu@unica.it>
2024-07-04 10:55:42 +0200

    Update getasn.py

--- getasn.py
+++ getasn.py
@@ -36,19 +36,19 @@ for line in lines:
             if item["Esito"]=="Si":
                 date=item["Data"]
                 break
-
-    page="https://asn23.cineca.it/pubblico/miur/esito/"+s1+"%252F"+s2+"/1/1"
-    l=getfulllist(page,s1+s2)
-    if len(l)>0:
-        check=True
-        res1=evstats(l)
-        if 'res1' not in locals():
-            res1="-"
-        if date!=None:
-            for item in l:
-                if item["Esito"]=="Si":
-                    date=item["Data"]
-                    break
+    if (!check):
+        page="https://asn23.cineca.it/pubblico/miur/esito/"+s1+"%252F"+s2+"/1/1"
+        l=getfulllist(page,s1+s2)
+        if len(l)>0:
+            check=True
+            res1=evstats(l)
+            if 'res1' not in locals():
+                res1="-"
+            if date!=None:
+                for item in l:
+                    if item["Esito"]=="Si":
+                        date=item["Data"]
+                        break
 
     if check:
         dates.append(date)
